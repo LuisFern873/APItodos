097d0a347721447b8ed92bfed8a1b670aa426706
Luis Fernando Mendez Lazaro <luis.mendez.l@utec.edu.pe>
2022-06-12 22:33:00 -0500

    init

---
+++ backend/server/__init__.py
@@ -0,0 +1,102 @@
+# Environment variables
+# $env:FLASK_APP = "server"
+# set FLASK_ENV=development
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS, cross_origin
+from models import setup_db, Todo, TodoList
+
+TODOS_PER_PAGE = 5
+
+def paginate_todos(request, selection):
+  page = request.args.get('page', 1, type = int)
+  
+  todos = [todo.format() for todo in selection]
+
+  start = (page - 1)*TODOS_PER_PAGE
+  end = start + TODOS_PER_PAGE
+
+  current_todos = todos[start:end]
+
+  return current_todos
+
+def create_app(test_config = None):
+  app = Flask(__name__)
+  setup_db(app)
+
+  CORS(app, origins = "http://127.0.0.1:5500/templates/index.html", max_age = 10)
+
+  @app.after_request
+  def after_resquest(response):
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorizations, true')
+      response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+      return response
+
+
+  @app.route("/todos", methods = ["GET"])
+  @cross_origin()
+  def get_todos():
+    selection = Todo.query.order_by('id').all()
+    todos = paginate_todos(request, selection)
+
+    if(len(todos) == 0):
+      abort(404)
+
+    return jsonify({
+      'success': True,
+      'todos': todos,
+      'total_todos': len(todos)
+    })
+  
+
+  @app.route("/todos", methods = ["POST"])
+  def create_todo():
+    # JSON BODY
+    body = request.get_json()
+    
+    description = body.get('description', None)
+    completed = body.get('completed', None)
+    list_id = body.get('list_id', None)
+
+    todo = Todo(description = description, completed = completed, list_id = list_id)
+
+    todo.insert()
+
+    selection = Todo.query.order_by('id').all()
+
+    current_todos = paginate_todos(request, selection)
+
+    return jsonify({
+        'success': True,
+        'created': todo.format(),
+        'todos': current_todos,
+        'total_todos': len(selection)
+    })
+
+  @app.route("/todos/<todo_id>", methods = ["PATCH"])
+  def update_todo(todo_id):
+    todo = Todo.query.filter_by(id = todo_id).one_or_none()
+
+    if todo == None:
+      abort(404)
+
+    body = request.get_json()
+
+    if 'description' in body:
+        todo.description = body.get('description')
+
+    todo.update()
+
+    return jsonify({
+        'success': True,
+        'id': todo_id
+    })
+
+  @app.errorhandler(404)
+  def not_found(error):
+      return jsonify({
+          'success': False,
+          'code': 404,
+          'message': 'resource not found'
+      }), 404
+
+  return app
---
+++ backend/test.py
@@ -0,0 +1,48 @@
+import unittest
+import json
+from server import create_app
+from models import setup_db, Todo, TodoList
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        # self: Propiedades de la clase
+        # La instancia de la aplicación y un cliente
+        self.app = create_app()
+        self.client = self.app.test_client
+
+        # CREATING A NEW DATABASE FOR TESTING
+        self.database_path = 'postgresql://postgres:conejowas12345@localhost:5432/todoapp10_test'
+
+        setup_db(self.app, self.database_path) # setup_db(app, database_path)
+    
+    def test_get_todos_success(self):
+        response = self.client().get('/todos')
+        print("Response.data (BSON): ", response.data) # Binary JSON (BSON)
+        print("Response.status_code: ", response.status_code)
+        # Dedeserializar (turn binary format into data)
+        data = json.loads(response.data)
+        print("Response.data (JSON): ", data)
+        # TESTING
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+
+    def test_update_todo_success(self):
+        response = self.client().patch('/todos/1', json = {'description': 'HIT ON GIRL'})
+        data = json.loads(response.data)
+
+        print("Response.data (JSON): ", data)
+
+        # TESTING
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['id'], '1')
+
+# python -m unittest
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
